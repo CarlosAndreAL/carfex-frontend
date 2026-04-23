44c1b6eca73018efc8c662cf178fcb7bd0c16b7b
CarlosAndreAL <carlosandrelemosaldev@gmail.com>
2026-04-23 04:24:29 -0300

    fix: upi url producao render

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,6 @@
-const API_URL = "http://localhost:3001";
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3001"
+    : "https://carfex-backend-docker.onrender.com";
+
 export default API_URL;
