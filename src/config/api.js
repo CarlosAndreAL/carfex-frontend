3942a6b424f0c571232b205c71e20929b0d66503
CarlosAndreAL <carlosandrelemosaldev@gmail.com>
2026-04-27 23:04:38 -0300

    sei la

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 const API_URL =
-  import.meta.env.MODE === "development"
+  import.meta.env.VITE_API_URL ||
+  (import.meta.env.MODE === "development"
     ? "http://localhost:3001"
-    : "https://carfex-backend-docker.onrender.com";
+    : "https://carfex-backend-docker.onrender.com");
 
 export default API_URL;
