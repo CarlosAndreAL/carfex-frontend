4784ac96f247281ae752980d72eeddb22b423633
CarlosAndreAL <carlosandrealdev@gmail.com>
2026-05-22 13:16:18 -0300

    corrigindo api vps

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,3 @@
-const API_URL =
-  (import.meta.env.MODE === "development"
-    ? "http://localhost:3001"
-    : "https://carfex-backend-docker.onrender.com");
+const API_URL = "http://2.24.117.116:3001";
 
 export default API_URL;
